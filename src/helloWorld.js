04bd0ff7adf23cb33f1efb495da52ccc7e8050a6
Seth Orell <seth.orell@gmail.com>
2022-10-16 20:19:48 -0500

    Add middy

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const middy = require('@middy/core');
+
 const hello = async (event) => {
   return { message: 'Hello from your JS template!', event };
 };
 
-module.exports = { hello };
+module.exports = {
+  hello: middy(hello),
+};
