15450c454358d55cbf4bfd8e7bb7b48245e6d9b1
Seth Orell <seth.orell@gmail.com>
2022-10-16 19:24:55 -0500

    Add jest-extended

--- test/identity.unit.test.js
+++ test/identity.unit.test.js
@@ -1,5 +1,11 @@
-describe('A', () => {
-  it('is A', () => {
+describe('Aristotle says', () => {
+  it('A is A', () => {
     expect('A').toBe('A');
   });
+});
+
+describe('Using jest-extended assertion', () => {
+  it('should execute', () => {
+    expect().pass('jest-extended is working');
+  })
 });
