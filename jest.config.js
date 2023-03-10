50f9d34fe1d3b74d646439186c706b1e2222831e
Seth Orell <seth.orell@gmail.com>
2023-03-10 15:31:45 -0600

    Use esbuild for jest and show import

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['jest-extended/all'],
+  transform: {
+    '^.+\\.jsx?$': 'esbuild-jest',
+  },
 };
--- test/identity.unit.test.js
+++ test/identity.unit.test.js
@@ -1,5 +1,8 @@
+import Logger from '@dazn/lambda-powertools-logger';
+
 describe('Aristotle says', () => {
   it('A is A', () => {
+    Logger.debug('aaaaaa');
     expect('A').toBe('A');
   });
 });
