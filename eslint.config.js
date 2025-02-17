8a98cbeba3a47a926f707f31fe10be840477e6d4
Seth Orell <seth.orell@gmail.com>
2025-02-17 10:41:56 -0600

    Start work on migrating to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+// eslint-disable import/no-extraneous-dependencies
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import noOnlyTests from 'eslint-plugin-no-only-tests';
+
+// Resolve __dirname in ESM
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const compat = new FlatCompat({
+  baseDirectory: dirname, // Optional, default to process.cwd()
+});
+
+export default [
+  // Migrate extends using FlatCompat
+  ...compat.extends('airbnb-base', 'prettier'),
+
+  // Migrate env using FlatCompat
+  ...compat.env({
+    jest: true,
+    es2021: true,
+    node: true,
+  }),
+
+  // Migrate plugins using FlatCompat and fixupPluginRoles for compatibility
+  // ...compat.plugins('older plugins'),
+  {
+    ignores: [
+      'node_modules/*',
+      '.serverless/*',
+      'eslint.config.js',
+    ]
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      }
+    },
+    // env: {
+    //   jest: true,
+    //   es2021: true,
+    //   node: true,
+    // },
+    // extends: ['prettier'],
+    rules: {
+      'import/extensions': 0,
+      'no-use-before-define': 'off',
+      'no-only-tests/no-only-tests': 'error',
+      'no-console': 2,
+    },
+    plugins: {
+      "no-only-tests": noOnlyTests,
+    }
+  }
+]
